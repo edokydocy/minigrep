486c9df7eb57ef28904915f3e0d89292bbaa9f13
edokydocy <edokydocy@gmail.com>
2025-05-21 23:55:40 -0700

    feat: parser nailed

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,25 @@ fn main() {
     dbg!(&args);    // cannot use "args" here. Moved
 
     // save variables
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = parse_config(&args);
     
-    println!("Searching for query: {query}");
-    println!("In file {file_path}");
+    println!("Searching for query: {}", config.query);
+    println!("In file {}", config.file_path);
 
     // print out contents
-    let content = fs::read_to_string(file_path)
+    let content = fs::read_to_string(config.file_path)
             .expect("should be able to read the file");
     
     println!("With a content:\n{content}");
+}
+
+struct Config{
+    query: String,
+    file_path: String
+}
+
+fn parse_config(args: &[String]) -> Config{
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+    Config{query, file_path}
 }
